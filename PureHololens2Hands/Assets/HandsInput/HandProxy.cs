a3f3c2d81cee0a9a242329154feff52598c49110
Greg Bahm <grbahm@microsoft.com>
2021-06-20 23:16:57 -0700

    Updating to no longer use the package and instead use the WIndows Mixed Reality Plugin.

--- PureHololens2Hands/Assets/HandsInput/HandProxy.cs
+++ PureHololens2Hands/Assets/HandsInput/HandProxy.cs
@@ -88,7 +88,7 @@ public class HandProxy
     {
         get
         {
-            if(allJoints == null)
+            if (allJoints == null)
             {
                 allJoints = GetAllJoints();
             }
@@ -98,6 +98,10 @@ public class HandProxy
 
     public Vector3 ModelFingerPointing = new Vector3(0, 0, 0);
     public Vector3 ModelPalmFacing = new Vector3(0, 0, 0);
+
+    public Vector3 WristPositionOffset = new Vector3(.06f, 0, 0);
+    public Vector3 WristRotationOffset = new Vector3(-30, 90, -90);
+
 
     private Transform[] allJoints;
     private Transform[] GetAllJoints()
--- PureHololens2Hands/Assets/HandsInput/Hands.cs
+++ PureHololens2Hands/Assets/HandsInput/Hands.cs
@@ -1,60 +1,16 @@
-﻿
-#if UNITY_WSA
-using System;
-using System.Collections;
-using System.Runtime.InteropServices;
-using UnityEngine;
-using UnityEngine.XR.WSA;
-using UnityEngine.XR.WSA.Input;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using System.Linq;
-#if WINDOWS_UWP
-using Windows.Perception;
-using Windows.Perception.People;
-using Windows.Perception.Spatial;
-using Windows.UI.Input.Spatial;
-#else
-using Microsoft.Windows.Perception;
-using Microsoft.Windows.Perception.People;
-using Microsoft.Windows.Perception.Spatial;
-using Microsoft.Windows.UI.Input.Spatial;
-#endif
-#endif
+using UnityEngine.XR;
+using System;
 
 public class Hands : MonoBehaviour
 {
-    #if UNITY_WSA
-    private static readonly HandJointKind[] jointIndices = new HandJointKind[]
-    {
-        HandJointKind.Palm,
-        HandJointKind.Wrist,
-        HandJointKind.ThumbMetacarpal,
-        HandJointKind.ThumbProximal,
-        HandJointKind.ThumbDistal,
-        HandJointKind.ThumbTip,
-        HandJointKind.IndexMetacarpal,
-        HandJointKind.IndexProximal,
-        HandJointKind.IndexIntermediate,
-        HandJointKind.IndexDistal,
-        HandJointKind.IndexTip,
-        HandJointKind.MiddleMetacarpal,
-        HandJointKind.MiddleProximal,
-        HandJointKind.MiddleIntermediate,
-        HandJointKind.MiddleDistal,
-        HandJointKind.MiddleTip,
-        HandJointKind.RingMetacarpal,
-        HandJointKind.RingProximal,
-        HandJointKind.RingIntermediate,
-        HandJointKind.RingDistal,
-        HandJointKind.RingTip,
-        HandJointKind.LittleMetacarpal,
-        HandJointKind.LittleProximal,
-        HandJointKind.LittleIntermediate,
-        HandJointKind.LittleDistal,
-        HandJointKind.LittleTip
-    };
-    private readonly JointPose[] jointPoses = new JointPose[jointIndices.Length];
+    public static Hands Instance { get; private set; }
 
+    private void Awake()
+    {
+        Instance = this;
+    }
     public HandProxy LeftHandProxy => this.leftHandProxy;
     [SerializeField]
     private HandProxy leftHandProxy;
@@ -62,140 +18,92 @@ public class Hands : MonoBehaviour
     [SerializeField]
     private HandProxy rightHandProxy;
 
-#if NETFX_CORE
-        [DllImport("DotNetNativeWorkaround.dll", EntryPoint = "MarshalIInspectable")]
-        private static extern void GetSpatialCoordinateSystem(IntPtr nativePtr, out SpatialCoordinateSystem coordinateSystem);
-
-        public static SpatialCoordinateSystem SpatialCoordinateSystem => spatialCoordinateSystem ?? (spatialCoordinateSystem = GetSpatialCoordinateSystem(WorldManager.GetNativeISpatialCoordinateSystemPtr()));
-
-        /// <summary>
-        /// Helps marshal WinRT IInspectable objects that have been passed to managed code as an IntPtr.
-        /// </summary>
-        /// <remarks>
-        /// On .NET Native, IInspectable pointers cannot be marshaled from native to managed code using Marshal.GetObjectForIUnknown.
-        /// This class calls into a native method that specifically marshals the type as a specific WinRT interface, which
-        /// is supported by the marshaller on both .NET Core and .NET Native.
-        /// Please see https://microsoft.github.io/MixedRealityToolkit-Unity/Documentation/InputSystem/HandTracking.html#net-native for more info.
-        /// </remarks>
-        private static SpatialCoordinateSystem GetSpatialCoordinateSystem(IntPtr nativePtr)
-        {
-            try
-            {
-                GetSpatialCoordinateSystem(nativePtr, out SpatialCoordinateSystem coordinateSystem);
-                return coordinateSystem;
-            }
-            catch
-            {
-                UnityEngine.Debug.LogError("Call to the DotNetNativeWorkaround plug-in failed. The plug-in is required for correct behavior when using .NET Native compilation");
-                return Marshal.GetObjectForIUnknown(nativePtr) as SpatialCoordinateSystem;
-            }
-        }
-#elif WINDOWS_UWP
-        public static SpatialCoordinateSystem SpatialCoordinateSystem => spatialCoordinateSystem ?? (spatialCoordinateSystem = Marshal.GetObjectForIUnknown(WorldManager.GetNativeISpatialCoordinateSystemPtr()) as SpatialCoordinateSystem);
-#else
-    public static SpatialCoordinateSystem SpatialCoordinateSystem
-    {
-        get
-        {
-            var spatialCoordinateSystemPtr = WorldManager.GetNativeISpatialCoordinateSystemPtr();
-            if (spatialCoordinateSystem == null && spatialCoordinateSystemPtr != System.IntPtr.Zero)
-            {
-                spatialCoordinateSystem = SpatialCoordinateSystem.FromNativePtr(WorldManager.GetNativeISpatialCoordinateSystemPtr());
-            }
-            return spatialCoordinateSystem;
-        }
-    }
-#endif
-    private static SpatialCoordinateSystem spatialCoordinateSystem = null;
-    
-    private SpatialInteractionManager SpatialInteractionManager
-    {
-        get
-        {
-            if (spatialInteractionManager == null)
-            {
-                UnityEngine.WSA.Application.InvokeOnUIThread(() =>
-                {
-                    spatialInteractionManager = SpatialInteractionManager.GetForCurrentView();
-                }, true);
-            }
-
-            return spatialInteractionManager;
-        }
-    }
-
-    private SpatialInteractionManager spatialInteractionManager = null;
-
-    private void Start()
-    {
-        InteractionManager.InteractionSourceDetected += InteractionManager_InteractionSourceDetected;
-    }
-
-    private void InteractionManager_InteractionSourceDetected(InteractionSourceDetectedEventArgs obj)
-    {
-        UpdateHands();
-    }
-
     private void Update()
     {
-        if(spatialInteractionManager != null)
-        {
-            UpdateHands();
-        }
+        UpdateHand(XRNode.LeftHand, leftHandProxy);
+        UpdateHand(XRNode.RightHand, rightHandProxy);
     }
 
-    private void UpdateHands()
+    private void UpdateHand(XRNode node, HandProxy proxy)
     {
-        PerceptionTimestamp perceptionTimestamp = PerceptionTimestampHelper.FromHistoricalTargetTime(DateTimeOffset.Now);
-        IReadOnlyList<SpatialInteractionSourceState> sources = SpatialInteractionManager?.GetDetectedSourcesAtTimestamp(perceptionTimestamp);
-
-        foreach (SpatialInteractionSourceState sourceState in sources)
+        List<InputDevice> devices = new List<InputDevice>(); // Gosh I hate this API...
+        InputDevices.GetDevicesAtXRNode(node, devices);
+        if (devices.Count == 1)
         {
-            HandPose handPose = sourceState.TryGetHandPose();
-            if (handPose != null && handPose.TryGetJoints(SpatialCoordinateSystem, jointIndices, jointPoses))
-            {
-                SpatialInteractionSourceHandedness handIndex = sourceState.Source.Handedness;
-                if(handIndex == SpatialInteractionSourceHandedness.Left)
-                {
-                    ApplyTransforms(leftHandProxy, jointPoses);
-                }
-                else
-                {
-                    ApplyTransforms(rightHandProxy, jointPoses);
-                }
-            }
+            UpdateHand(devices[0], proxy);
         }
     }
 
-    private void ApplyTransforms(HandProxy hand, JointPose[] jointPoses)
+    private void UpdateHand(InputDevice handDevice, HandProxy handProxy)
     {
-        for (int i = 0; i < jointPoses.Length; i++)
+        UpdatePalm(handDevice, handProxy);
+        //UpdateHandJoint(handDevice, handProxy, "BoneThumbMetacarpal",      handProxy.ThumbMetacarpal);
+        UpdateHandJoint(handDevice, handProxy, "BoneThumbProximal", handProxy.ThumbMetacarpal);
+        UpdateHandJoint(handDevice, handProxy, "BoneThumbIntermediate", handProxy.ThumbProximal);
+        UpdateHandJoint(handDevice, handProxy, "BoneThumbDistal", handProxy.ThumbDistal);
+        UpdateHandJoint(handDevice, handProxy, "BoneThumbTip", handProxy.ThumbTip);
+        UpdateHandJoint(handDevice, handProxy, "BoneIndexMetacarpal", handProxy.IndexMetacarpal);
+        UpdateHandJoint(handDevice, handProxy, "BoneIndexProximal", handProxy.IndexProximal);
+        UpdateHandJoint(handDevice, handProxy, "BoneIndexIntermediate", handProxy.IndexIntermediate);
+        UpdateHandJoint(handDevice, handProxy, "BoneIndexDistal", handProxy.IndexDistal);
+        UpdateHandJoint(handDevice, handProxy, "BoneIndexTip", handProxy.IndexTip);
+        UpdateHandJoint(handDevice, handProxy, "BoneMiddleMetacarpal", handProxy.MiddleMetacarpal);
+        UpdateHandJoint(handDevice, handProxy, "BoneMiddleProximal", handProxy.MiddleProximal);
+        UpdateHandJoint(handDevice, handProxy, "BoneMiddleIntermediate", handProxy.MiddleIntermediate);
+        UpdateHandJoint(handDevice, handProxy, "BoneMiddleDistal", handProxy.MiddleDistal);
+        UpdateHandJoint(handDevice, handProxy, "BoneMiddleTip", handProxy.MiddleTip);
+        UpdateHandJoint(handDevice, handProxy, "BoneRingMetacarpal", handProxy.RingMetacarpal);
+        UpdateHandJoint(handDevice, handProxy, "BoneRingProximal", handProxy.RingProximal);
+        UpdateHandJoint(handDevice, handProxy, "BoneRingIntermediate", handProxy.RingIntermediate);
+        UpdateHandJoint(handDevice, handProxy, "BoneRingDistal", handProxy.RingDistal);
+        UpdateHandJoint(handDevice, handProxy, "BoneRingTip", handProxy.RingTip);
+        UpdateHandJoint(handDevice, handProxy, "BonePinkyMetacarpal", handProxy.LittleMetacarpal);
+        UpdateHandJoint(handDevice, handProxy, "BonePinkyProximal", handProxy.LittleProximal);
+        UpdateHandJoint(handDevice, handProxy, "BonePinkyIntermediate", handProxy.LittleIntermediate);
+        UpdateHandJoint(handDevice, handProxy, "BonePinkyDistal", handProxy.LittleDistal);
+        UpdateHandJoint(handDevice, handProxy, "BonePinkyTip", handProxy.LittleTip);
+    }
+
+    private void UpdatePalm(InputDevice handDevice, HandProxy handProxy)
+    {
+        InputFeatureUsage<Quaternion> rotUsage = new InputFeatureUsage<Quaternion>("DeviceRotation");
+        Quaternion handRot;
+        if (handDevice.TryGetFeatureValue(rotUsage, out handRot))
         {
-            ApplyPose(hand.AllJoints[i], jointPoses[i], hand);
+            handProxy.Wrist.rotation = handRot * Reorientation(handProxy);
+            handProxy.Wrist.rotation *= Quaternion.Euler(handProxy.WristRotationOffset);
+        }
+
+        InputFeatureUsage<Vector3> posUsage = new InputFeatureUsage<Vector3>("DevicePosition");
+        Vector3 handPos;
+        if (handDevice.TryGetFeatureValue(posUsage, out handPos))
+        {
+            handProxy.Wrist.position = handPos;
+            Vector3 offset = handProxy.Wrist.rotation * handProxy.WristPositionOffset;
+            handProxy.Wrist.localPosition += offset;
         }
     }
 
-    private void ApplyPose(Transform transform, JointPose jointPose, HandProxy hand)
+    private void UpdateHandJoint(InputDevice handDevice, HandProxy hand, string name, Transform proxy)
     {
-        if(transform != null)
+        if (proxy == null)
+            return;
+
+        InputFeatureUsage<Bone> usage = new InputFeatureUsage<Bone>(name);
+        Bone bone;
+        if (handDevice.TryGetFeatureValue(usage, out bone))
         {
-            transform.position = SystemVector3ToUnity(jointPose.Position);
-            transform.rotation = SystemQuaternionToUnity(jointPose.Orientation) * Reorientation(hand);
+            Vector3 jointPos;
+            if (bone.TryGetPosition(out jointPos))
+                proxy.position = jointPos;
+            Quaternion jointRot;
+            if (bone.TryGetRotation(out jointRot))
+                proxy.rotation = jointRot * Reorientation(hand);
         }
     }
+
     private Quaternion Reorientation(HandProxy hand)
     {
         return Quaternion.Inverse(Quaternion.LookRotation(hand.ModelFingerPointing, -hand.ModelPalmFacing));
     }
-
-    public static UnityEngine.Vector3 SystemVector3ToUnity(System.Numerics.Vector3 vector)
-    {
-        return new UnityEngine.Vector3(vector.X, vector.Y, -vector.Z);
-    }
-
-    public static UnityEngine.Quaternion SystemQuaternionToUnity(System.Numerics.Quaternion quaternion)
-    {
-        return new UnityEngine.Quaternion(-quaternion.X, -quaternion.Y, quaternion.Z, quaternion.W);
-    }
-#endif
 }
